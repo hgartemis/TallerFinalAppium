650554a387aa99ab01668b8df2ed49d82be90c56
Juan De Dios <jbaudazio@gmail.com>
2021-04-28 13:45:33 -0500

    Metodos verifyAdditionId,verifyAdditionXpath,verifyOrderBy,AgregarItem

--- src/test/java/WhenDo.java
+++ src/test/java/WhenDo.java
@@ -20,9 +20,21 @@ public class WhenDo {
     private AppiumDriver driver;
     @Before
     public void initializeAppium() throws MalformedURLException {
+
+        // Juan de Dios
+        /*
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName","nubia Z17 lite");
         capabilities.setCapability("platformVersion","7.1.1");
+        capabilities.setCapability("appPackage","com.vrproductiveapps.whendo");
+        capabilities.setCapability("appActivity",".ui.HomeActivity");
+        capabilities.setCapability("platformName","Android");
+        */
+
+        // Humberto Guadalupe
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","Android SDK built for x86");
+        capabilities.setCapability("platformVersion","8.1.0");
         capabilities.setCapability("appPackage","com.vrproductiveapps.whendo");
         capabilities.setCapability("appActivity",".ui.HomeActivity");
         capabilities.setCapability("platformName","Android");
@@ -34,7 +46,7 @@ public class WhenDo {
     }
 
     @Test
-    public void verifyAddition() throws InterruptedException {
+    public void verifyAdditionId() throws InterruptedException {
 
         String titulo="Taller2";
 
@@ -50,6 +62,94 @@ public class WhenDo {
         Assert.assertTrue("ERROR! tarea no creada",driver.findElement(By.xpath("//android.widget.TextView[@text='"+titulo+"']")).isDisplayed());
 
     }
+
+    @Test
+    public void verifyAdditionXpath() throws InterruptedException {
+
+        String titulo="TallerXPath";
+        String notes="Forma de registrar notes en la tareas";
+
+        // click Agregar
+        driver.findElement(By.xpath("//android.widget.ImageButton[@index='2']")).click();
+
+        //Ingresar Tarea
+        driver.findElement(By.xpath("//android.widget.EditText[@text='Title']")).sendKeys(titulo);
+
+        //Ingresar Notes
+        driver.findElement(By.xpath("//android.widget.EditText[@text='Notes']")).sendKeys(notes);
+
+        // click Guardar
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Save']")).click();
+
+        // verificacion, verificar que la tarea este creada
+        Thread.sleep(2000);
+        Assert.assertTrue("ERROR! tarea no creada",driver.findElement(By.xpath("//android.widget.TextView[@text='"+titulo+"']")).isDisplayed());
+
+    }
+
+    @Test
+    public void verifyOrderBy() throws InterruptedException {
+
+
+        String tarea0 = "Ir al mercado";
+        String tarea1 = "Salir a jugar futbol";
+        String tarea2 = "Dormir";
+        String tarea3 = "Cocinar";
+        String tarea4 = "Hacer la tarea de Appium";
+        String note0 = "Todos los dias a las 10am";
+        String note1 = "Sabados de 5 a 8 pm";
+        String note2 = "desde las 10pm a las 7am";
+        String note3 = "Desde las 11am a las 2pm";
+        String note4 = "De lunes a viernes";
+
+        for(int i=0; i<5; i++) {
+
+            String t = ""; String n = "";
+            switch (i) {
+                case 0:t = tarea0; n = note0;break;
+                case 1:t = tarea1; n = note1;break;
+                case 2:t = tarea2; n = note2;break;
+                case 3:t = tarea3; n = note3;break;
+                case 4:t = tarea4; n = note4;break;
+            }
+
+            // Agregando Tareas
+            AgregarItem(t , n);
+            Thread.sleep(1000);
+
+        }
+
+        // verificacion, verificar que la tarea este creada
+        Thread.sleep(2000);
+        //Assert.assertTrue("ERROR! tarea no creada",driver.findElement(By.xpath("//android.widget.TextView[@text='"+titulo+"']")).isDisplayed());
+
+        // click en SortBy
+        driver.findElement(By.id("com.vrproductiveapps.whendo:id/home_header_icon")).click();
+
+        // click en Orden Alfabetico
+        driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='Alphabetic']")).click();
+
+        // Esperando a se vizualiza
+        Thread.sleep(3000);
+
+    }
+
+    public void AgregarItem(String tarea, String notes) throws InterruptedException {
+
+        // click Agregar
+        driver.findElement(By.id("com.vrproductiveapps.whendo:id/fab")).click();
+        //Ingresar Tarea
+        driver.findElement(By.id("com.vrproductiveapps.whendo:id/noteTextTitle")).sendKeys(tarea);
+
+        //Ingresar Notes
+        driver.findElement(By.xpath("//android.widget.EditText[@text='Notes']")).sendKeys(notes);
+
+        // click Guardar
+        driver.findElement(By.id("com.vrproductiveapps.whendo:id/saveItem")).click();
+
+    }
+
+
 
     @After
     public void closeApp(){
